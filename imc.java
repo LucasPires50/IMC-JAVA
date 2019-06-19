246e9bc7bdb71b3073010c2bf251c32774c72dd3
Lucas Albuquerque Pires <50931185+LucasPires50@users.noreply.github.com>
2019-06-19 09:41:13 -0300

    Update imc.java

--- imc.java
+++ imc.java
@@ -1,19 +1,24 @@
-
-package javaapplication3;
-
-import java.util.Scanner;
-
-public class JavaApplication3 {
-    public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
+Scanner leia = new Scanner(System.in);
         
-        System.out.println("informe o peso");
-        double peso = leia.nextDouble();
-        
-        System.out.println("informe a altura");
+        System.out.println("Infomer a altura: ");
         double altura = leia.nextDouble();
         
-        double resultado = peso / (altura * altura);
-        System.out.println(resultado);
-    }
-}
+        System.out.println("Informe o peso: ");
+        double peso = leia.nextDouble();
+        
+        double imc = peso / (altura * altura);
+        System.out.println("Imc: "+imc);
+        
+        if (imc < 18.5){
+            System.out.println("Abaixo do peso!");
+        } else if(imc >= 18.5 & imc <= 24.9){
+            System.out.println("Peso Normal!");
+        } else if(imc >= 25 & imc <= 29.9){
+            System.out.println("Sobrepeso!");
+        } else if(imc >= 30 & imc <= 34.9){
+            System.out.println("Obesidade Grau 1!");
+        } else if(imc >= 35 & imc <= 39.9){
+            System.out.println("Obesidade Grau 2!");
+        } else {
+            System.out.println("Obesidade Grau 3!");
+        }
